faa3df9f20d36bab1616250b4cf4c11bd2c5589d
tarek339 <tarekjassine@gmail.com>
2025-06-09 20:52:06 +0200

    Refactor internationalization setup by removing old message files and restructuring message imports for home and about sections

---
+++ messages/index.ts
@@ -0,0 +1,15 @@
+import homeDE from "./home/de.json";
+import homeEN from "./home/en.json";
+import aboutDE from "./about/de.json";
+import aboutEN from "./about/en.json";
+
+const de = {
+  home: homeDE.home,
+  about: aboutDE.about,
+};
+const en = {
+  home: homeEN.home,
+  about: aboutEN.about,
+};
+
+export { en, de };
--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -4,8 +4,10 @@ import { getRequestConfig } from "next-intl/server";
 export default getRequestConfig(async () => {
   const locale = await getUserLocale();
 
+  const { de, en } = await import("../../messages/index");
+
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    messages: locale === "de" ? de : en,
   };
 });
